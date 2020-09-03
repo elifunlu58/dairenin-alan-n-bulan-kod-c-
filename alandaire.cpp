12f89141b93615a29d0ed011b7455aae1c1fa8ea
elif <48827713+elifunlu58@users.noreply.github.com>
2020-09-03 20:09:10 +0300

    Add files via upload

---
+++ alandaire.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main() 
+{
+double r,a,c;
+const double pi=3.14;
+cout<<"r:";
+cin>>r;
+a=pi*pow(r,2);
+c=2*pi*r;
+cout<<"alan="<<a<<"\t cevre="<<c;
+return 0;
+	
+
+}
